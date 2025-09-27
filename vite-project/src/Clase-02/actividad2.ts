8d42a0c962a62e35c3f400299c0253feb7f9baa1
Sebastianpel <sebastianparrapellaton@gmail.com>
2025-09-27 10:58:09 -0500

    wuju

--- vite-project/src/Clase-02/actividad2.ts
+++ vite-project/src/Clase-02/actividad2.ts
@@ -4,38 +4,52 @@ interface Comida {
     esVegana: boolean;
 }
 
-const pizza: Comida {
+const pizza: Comida = {
     nombre: "Pizza",
     calorias: 20,
     esVegana: false
     }
-const ensalada: Comida {
+const ensalada: Comida = {
     nombre: "Ensalada",
     calorias: 30,
     esVegana: true
 }
-const hamburguesa: Comida = new Comida (
-    "Hamburguesa", 200, false
-)
-console.log(comida)
+const hamburguesa: Comida = {
+    nombre:"Hamburguesa",
+    calorias: 200,
+    esVegana: false
+}
 
 export class ComidaService {
     constructor() {
     }
-    mostrarInfo(comida: Comida) {
-        console.log(comida.nombre);
+    mostrarInfo(comida: Comida): void {
+        const {nombre, calorias, esVegana} = comida;
+        console.log(`Nombre: ${nombre}, calorias: ${calorias}, esVegana: ${esVegana}`);
     }
 
     modificarCalorias(comida: Comida, nuevasCalorias: number): Comida {
-        return nuevasCalorias = calorias;
+        return {...comida, calorias: nuevasCalorias};
     }
 }
 
 class NotificadorComida {
-    constructor(ComidaService) {
-        notificar(comida: Comida)
-        {
-            console.log(`La comida es: " ${this.mostrarInfo}`)
-        }
+    constructor(private ComidaService: ComidaService) {}
+        notificar(comida: Comida): void{
+            console.log("Notificando Comida...");
+
+    this.ComidaService.mostrarInfo(comida);
+    console.log();
     }
 }
+
+const service = new ComidaService();
+const notificador = new NotificadorComida(service);
+
+notificador.notificar(pizza);
+notificador.notificar(ensalada);
+notificador.notificar(hamburguesa);
+
+const pizzaModificada = service.modificarCalorias(pizza, 500);
+
+notificador.notificar(pizzaModificada);
